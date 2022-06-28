c5d83f41fbbf501636a8b012807fbe0989ad7dfd
z0w1non <z0w1non@gmail.com>
2022-06-28 22:22:07 +0900

    Update main.cpp

--- shogipp/main.cpp
+++ shogipp/main.cpp
@@ -1,7 +1,8 @@
 #include "shogipp.hpp"
 
-int main(int ac, char ** av)
+int main(int ac, const char ** av)
 {
     using namespace shogipp;
-    do_taikyoku<niwatori_dou_evaluator_t, random_evaluator_t>();
+    //do_taikyoku<niwatori_dou_evaluator_t, random_evaluator_t>();
+    parse_command_line(ac, av);
 }

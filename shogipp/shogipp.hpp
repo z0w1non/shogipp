abb74ab9275efbb46a4ec75a14582983cde97496
z0w1non <z0w1non@gmail.com>
2022-07-11 00:35:15 +0900

    Update shogipp.hpp

--- shogipp/shogipp.hpp
+++ shogipp/shogipp.hpp
@@ -2163,7 +2163,7 @@ namespace shogipp
                     // Šù‚É‡‹î‚Æ‚µ‚Äg‚Á‚Ä‚¢‚é‹î‚ÍˆÚ“®‚Å‚«‚È‚¢B
                     auto aigoma_iter = aigoma_info.find(kiki.pos);
                     bool is_aigoma = aigoma_iter != aigoma_info.end();
-                    if (!is_aigoma)
+                    if (!is_aigoma || check_list.front().offset == kiki.offset * -1)
                         search_te_from_positions(result, kiki.pos, destination);
                 }
             }
